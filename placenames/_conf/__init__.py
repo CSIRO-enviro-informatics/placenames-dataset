3a59769a066bcf42231f44bf3976c975fd03c41c
Nicholas Car <nicholas.car@csiro.au>
2019-05-23 11:11:56 +1000

    getting real data from Shane's DB online

--- placenames/_conf/__init__.py
+++ placenames/_conf/__init__.py
@@ -1,6 +1,7 @@
 from os.path import dirname, realpath, join, abspath
-from .db import *
-from .secrets import *
+import os
+import psycopg2  # installed with LDFLAGS=-L/usr/local/opt/openssl/lib pip install psycopg2
+import psycopg2.extras
 
 
 APP_DIR = dirname(dirname(realpath(__file__)))
@@ -11,6 +12,21 @@ STATIC_DIR = join(dirname(dirname(abspath(__file__))), 'view', 'static')
 LOGFILE = APP_DIR + '/flask.log'
 DEBUG = True
 
+if os.environ['PLACE_NAMES_DB_CON'] is None:
+    print('You must set an environment variable for the DB connection called PLACE_NAMES_DB_CON')
+    exit()
+else:
+    PLACE_NAMES_DB_CON = os.environ['PLACE_NAMES_DB_CON']
 
 JURISDICTION_INSTANCE_URI_STEM = 'http://localhost:5000/jurisdiction/'
 GAZETTEER_INSTANCE_URI_STEM = 'http://localhost:5000/gazetteer/'
+
+
+def db_select(q):
+    try:
+        conn = psycopg2.connect(PLACE_NAMES_DB_CON)
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute(q)
+        return cur.fetchall()
+    except Exception as e:
+        print(e)
--- placenames/controller/routes.py
+++ placenames/controller/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, redirect, url_for, Response, render_template
 from placenames.model.placename import Placename
 from pyldapi import RegisterRenderer
+import placenames._conf as conf
 
 routes = Blueprint('controller', __name__)
 
@@ -10,26 +11,30 @@ def home():
     return render_template('home.html')
 
 
-@routes.route('/placenames/')
+@routes.route('/placename/')
 def placenames():
     # get the total register count from the XML API
     try:
-        # MAGIC NUMBER DUMMY COUNT
-        # no_of_items = 42
+        # get the register length from the online DB
+        no_of_items = conf.db_select('SELECT COUNT(*) FROM "PLACENAMES";')[0][0]
 
-        # page = request.values.get('page') if request.values.get('page') is not None else 1
-        # per_page = request.values.get('per_page') if request.values.get('per_page') is not None else 3
-        items = [
-            ('http://fake.com/1', 'Fake 1'),
-            'http://fake.com/2',
-            ('http://fake.com/3', 'Fake 3'),
-            ('http://fake.com/4', 'Fake 4'),
-            ('http://fake.com/5', 'Fake 5'),
-            ('http://fake.com/6', 'Fake 6'),
-        ]
+        page = request.values.get('page') if request.values.get('page') is not None else 1
+        per_page = request.values.get('per_page') if request.values.get('per_page') is not None else 30
+        offset = (page - 1) * per_page
+        items = []
+        q = '''
+            SELECT "ID", "NAME" FROM "PLACENAMES"
+            ORDER BY "ID"
+            OFFSET {}
+            LIMIT {}
+        '''.format(offset, per_page)
+        for item in conf.db_select(q):
+            items.append(
+                (item[0], item[1])
+            )
     except Exception as e:
         print(e)
-        return Response('The Samples Register is offline', mimetype='text/plain', status=500)
+        return Response('The Place Names Register is offline', mimetype='text/plain', status=500)
 
     return RegisterRenderer(
         request,
@@ -38,7 +43,7 @@ def placenames():
         'A register of Place Names',
         items,
         ['http://linked.data.gov.au/def/placenames/PlaceName'],
-        len(items)
+        no_of_items
     ).render()
 
 
